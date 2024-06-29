5871ff4ff4952c4edb1cd037bafcc7143570dc04
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-06-29 15:29:48 +0530

    setup React/Tailwind and initial Pages

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductList from './features/product-list/ProductList'
+import Counter from './features/counter/Counter'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
