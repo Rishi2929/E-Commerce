5d85b44ac88fcef5d1651712c91c62056aaec453
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-06-29 15:47:48 +0530

    Login and signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import ProductList from './features/product-list/ProductList'
 import Counter from './features/counter/Counter'
 import Home from './pages/Home'
+import Login from './features/auth/components/Login'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
 
   return (
     <div className='App'>
-      <Home />
+      {/* <Home /> */}
+      {/* <Login /> */}
+      <SignUpPage></SignUpPage>
     </div>
   )
 }
