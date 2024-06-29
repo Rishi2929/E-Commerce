16aa6fc553ab9bcc5d90801d3f11149c8f58b9db
Rishi2929 <128122653+Rishi2929@users.noreply.github.com>
2024-06-29 16:51:51 +0530

    Cart Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Home from './pages/Home'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import './App.css'
+import Cart from "./features/cart/Cart";
+import CartPage from "./pages/CartPage";
 
 
 
@@ -23,7 +25,10 @@ const router = createBrowserRouter([
     path: "/signup",
     element: <SignUpPage> </SignUpPage>,
   },
-
+  {
+    path: "/cart",
+    element: <CartPage> </CartPage>,
+  },
 ]);
 
 function App() {
